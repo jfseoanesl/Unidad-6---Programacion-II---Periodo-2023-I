225046063db5f20da9aed14a7f9d38e991497fbb
jfseoanesl <jafrasele@gmail.com>
2023-05-29 20:32:43 +0100

    Actualizacion

--- ProyectoRentaVehiculo/src/vista/Principal.java
+++ ProyectoRentaVehiculo/src/vista/Principal.java
@@ -6,10 +6,7 @@ package vista;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import logica.LogicRenta;
 import modelo.*;
 
